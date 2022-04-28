16417a4fd839f24747f298b77dc0d2768006da9e
Victor Moragues <30898324+vicmova@users.noreply.github.com>
2022-04-28 13:01:57 +0200

    Update http.js

--- http.js
+++ http.js
@@ -3,7 +3,7 @@ const http = require('http');
     http.createServer(function (req, res) {
        
          //https://es.wikipedia.org/wiki/Anexo:C%C3%B3digos_de_estado_HTTP.  
-         res.writeHead(200);
+         res.writeHead(200, {'content-type': 'text/html'});
 
          res.write('<h1>mensaje visible desde el navegador</h1>');
 
